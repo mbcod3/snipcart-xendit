504f95d00d00918d0f4c93c3a1e4cc6a25d05d5c
Muhammad Bilal <emailc123@gmail.com>
2021-08-27 22:10:24 +0500

    complete flow

---
+++ functions/payment.js
@@ -0,0 +1,36 @@
+exports.handler = async function(event) {
+  // Retrieve payment information (depends on how your application is made)
+  const requestBody = JSON.parse(event.body)
+
+  // Process the payment with the gateway of your choice here
+
+  // Confirm payment with the /payment endpoint
+  const response = await fetch(
+    "https://payment.snipcart.com/api/private/custom-payment-gateway/payment",
+    {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer <YOUR_SECRET_API_KEY>",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        paymentSessionId: requestBody.paymentSessionId,
+        state: requestBody.state,
+        error: requestBody.error,
+        transactionId: paymentId,
+        instructions:
+          "Your payment will appear on your statement in the coming days",
+        links: { refunds: `<YOUR_REFUND_URL>?transactionId=${paymentId}` },
+      }),
+    }
+  )
+
+  if (response.ok) {
+    const body = await response.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ ok: true, returnUrl: body.returnUrl }),
+    }
+  }
+}
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react"
+import { v4 as uuidv4 } from "uuid"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Checkout = () => {
   const [order, setOrder] = useState(null)
-
+  const sessionId = React.useRef(null)
   useEffect(() => {
     // Get public token from query string
     const publicToken = new URLSearchParams(window.location.search).get(
@@ -15,20 +16,37 @@ const Checkout = () => {
     fetch(
       `https://payment.snipcart.com/api/public/custom-payment-gateway/payment-session?publicToken=${publicToken}`
     )
-      .then(res => response.ok && res.json())
+      .then(res => res.ok && res.json())
       .then(data => {
-        console.log(data)
+        sessionId.current = data.id
+        setOrder(data.invoice.amount)
       })
   }, [])
+
+  // simulates successfull order
+  const paid = async () => {
+    const transactionId = uuidv4()
+    const response = await fetch("/.netlify/functions/payment", {
+      method: "POST",
+      body: JSON.stringify({
+        paymentSessionId: sessionId.current,
+        transactionId,
+        state: "processed",
+        error: null,
+      }),
+    })
+    const body = await response.json()
+
+    window.location.href = body.returnUrl
+  }
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <div className="container">
         <div className="text-center mt-5">
           <h2 className="with-underline">Checkout Page</h2>
-          {/* {order ? (
-
-          ) : 'loading'} */}
+          <p>{order ? order : ""}</p>
+          <button onClick={paid}>Paid</button>
         </div>
       </div>
     </Layout>
