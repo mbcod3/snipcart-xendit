d4981affe4fb11117e88100ad409fdf1fb02b636
Muhammad Bilal <emailc123@gmail.com>
2021-08-27 21:09:36 +0500

    step 2

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,14 +1,33 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Checkout = () => {
+const [order, setOrder] = useState(null)
+
+  useEffect(() => {
+    // Get public token from query string
+const publicToken = new URLSearchParams(window.location.search).get('publicToken')
+
+// Fetch payment session from API
+const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/payment-session?publicToken=${publicToken}`);
+
+// Retrieve body as JSON if the request's status code is successful
+if (response.ok) {
+    const paymentSession = await response.json()
+    console.log(paymentSession)
+}
+    
+  }, [])
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <div className="container">
         <div className="text-center mt-5">
           <h2 className="with-underline">Checkout Page</h2>
+          {/* {order ? (
+
+          ) : 'loading'} */}
         </div>
       </div>
     </Layout>
