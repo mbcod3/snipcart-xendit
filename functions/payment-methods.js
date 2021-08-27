40de29529fd4d841efbc7a191ee1727042d37b8a
Muhammad Bilal <emailc123@gmail.com>
2021-08-27 18:34:54 +0500

    added payment methods api

---
+++ functions/payment-methods.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch")
+
+exports.handler = async event => {
+  // Get request's body
+  const request = JSON.parse(event.body)
+
+  // Validate that the request is coming from Snipcart
+  const response = await fetch(
+    `https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`
+  )
+
+  // Return a 404 if the request is not from Snipcart
+  if (!response.ok)
+    return {
+      statusCode: 404,
+      body: "",
+    }
+
+  // Create a payment method list
+  let paymentMethodList = [
+    {
+      id: "xendit0765291",
+      name: "Xendit",
+      iconUrl: "https://snipcart-xendit.netlify.app/static/xendit-logo.png",
+      checkoutUrl: "https://snipcart-xendit.netlify.app/checkout",
+    },
+  ]
+
+  // Return successful status code and available payment methods
+  return {
+    statusCode: 200,
+    body: JSON.stringify(paymentMethodList),
+  }
+}
