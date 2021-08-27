5d52fb53ef43929ca8b779339d613ff077fe8c7e
Muhammad Bilal <emailc123@gmail.com>
2021-08-27 20:48:42 +0500

    chekcout page debug5

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,14 +1,8 @@
-import axios from "axios"
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Checkout = () => {
-  const t = async () =>
-    await axios.post("/.netlify/functions/payment-methods", {
-      PublicToken: "1",
-    })
-  console.log(t())
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
