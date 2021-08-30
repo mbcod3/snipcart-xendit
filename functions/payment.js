e19254cee0f1edcf16ccfd456ab2b38f80c45738
Muhammad Bilal <emailc123@gmail.com>
2021-08-31 01:14:44 +0500

    testing 2

--- functions/payment.js
+++ functions/payment.js
@@ -5,14 +5,13 @@ const fetch = require("node-fetch")
 exports.handler = async function(event) {
   // Retrieve payment information (depends on how your application is made)
   const requestBody = JSON.parse(event.body)
-  let resp = "asd"
   // this key should never be on client side code. this is serverless function so its safe here
   // Dont forget to change this to yours
   const xenditSecretKey =
     "xnd_development_eqCZkBRK5NFf7IQrKawRshVG156og4tQUOiNTz8Sc5Cr0PTO1Isd6FPpRtXU0"
 
   // Payment processing with xendit
-  fetch("https://api.xendit.co/credit_card_charges", {
+  const resp = await fetch("https://api.xendit.co/credit_card_charges", {
     body: JSON.stringify({
       external_id: requestBody.transactionId,
       token_id: requestBody.xenditTokenId,
@@ -25,7 +24,7 @@ exports.handler = async function(event) {
     method: "POST",
   })
     .then(res => res.json())
-    .then(data => (resp = data))
+    .then(data => data)
   // Confirm payment with the /payment endpoint
   const response = await fetch(
     "https://payment.snipcart.com/api/private/custom-payment-gateway/payment",
